3266d3eeab59be4b3a5335ffc4416f0a0a55512f
Fernando Araujo <33295357+FernanPro18@users.noreply.github.com>
2018-09-20 21:09:40 -0500

    Delete Rutas.ts

--- src/app/Services/Rutas.ts
+++
@@ -1,5 +0,0 @@
-export default {
-    Registrar: 'https://livechat-backend-app.herokuapp.com/Registrar',
-    Login: 'https://livechat-backend-app.herokuapp.com/Login',
-    AuthEncrypt: 'SGEGEWGJEIERHERRKJSDÑLHJEWRKWJFERNANDOARAUJOELMEJORDETODOSLOSTIEMPOSTUPUTAMADRINASILOGRASESTOPERRAAAAAAAAAAAAAAAA2454557754224454'
-}
