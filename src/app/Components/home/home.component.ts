9810f03b150e80b6132a89366c95b9a6016781fb
Fernando Araujo <fernandoaraujo081@gmail.com>
2018-09-25 17:52:18 -0500

    Color

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -3,31 +3,43 @@ import { UsuariosService } from '../../Services/usuarios.service';
 import { Router } from '@angular/router';
 import { UsuarioLocal } from '../../Models/usuario.model';
 import * as swal from 'sweetalert';
+declare const $: any;
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  User: UsuarioLocal;
-  Valid: boolean;
+    User: UsuarioLocal;
+    Valid: boolean;
+    items: ['', '', '']
 
-  constructor(private userService: UsuariosService, private _Router: Router) {
-    this.userService.ObtenerUser().then(data => {
-      if (!data) {
-        this._Router.navigate(['/Login']);
-      } else {
-        this.User = new UsuarioLocal(data.Nombre, data.Email, data.Password, data.Foto, data.Token, 20, 25);
-      }
-    }).catch(err => {
-      this._Router.navigate(['/Login']);
-    });
-  }
+    constructor(private userService: UsuariosService, private _Router: Router) {
+        this.userService.ObtenerUser().then(data => {
+            if (!data) {
+                this._Router.navigate(['/Login']);
+            } else {
+                this.User = new UsuarioLocal(data.Nombre, data.Email, data.Password, data.Foto, data.Token, 20, 25);
+            }
+        }).catch(err => {
+            this._Router.navigate(['/Login']);
+        });
+    }
 
-  ngOnInit() {
+    ngOnInit() {
 
-  }
+    }
+
+    SideBar() {
+        $('.ui.sidebar')
+            .sidebar('setting', {
+                dimPage: false,
+                transition: 'scale down'
+            })
+        $('.ui.sidebar')
+            .sidebar('toggle')
+    }
 
 }
